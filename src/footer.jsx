6843dfe5c697500ebaafa6caf10fb269dc77c7fc
Srayan <srayanchakraborty.10@gmail.com>
2023-01-31 08:41:42 +0530

    This is the first commit

---
+++ src/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer(){
+
+    let date=new Date();
+    let year=date.getFullYear();
+    
+
+    return <footer>
+        <p>©Copyright {year}</p>
+    </footer>
+}
+
+export default Footer;
