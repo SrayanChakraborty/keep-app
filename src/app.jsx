d23249be387aa5ab533158adb5aa02c352f26d78
Srayan <srayanchakraborty.10@gmail.com>
2023-02-17 13:53:12 +0530

    First commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import Header from './header'
-import Footer from './footer'
-import Note from './note'
+import Header from './header';
+import Footer from './footer';
+import Note from './note';
+import notes from './notes.js';
+
+
+function createNote(note){
+    return(
+        <div>
+            <Note 
+                key={note.id}
+                heading={note.title}
+                content={note.content}
+            />
+        </div>
+
+    )
+}
 
 function App() {
     return <div>
         <Header />
-        <Note />
+        {notes.map(createNote)}
         <Footer />
     </div>
 
--- src/note.jsx
+++ src/note.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-function Note(){
-   
-    return <div className="note">
-        <h1>Heading</h1>
-        <p>paragraph</p>
+function Note(props) {
+
+    return (<div className="note">
+        <h1>{props.heading}</h1>
+        <p>{props.content}</p>
     </div>
-   
+    )
+
 }
 
 export default Note;
